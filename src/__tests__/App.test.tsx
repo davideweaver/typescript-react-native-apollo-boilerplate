e8e0ecd112d1a3be0fe51aef86ff2ca789add2a1
Dave Weaver <dave@markkup.com>
2019-01-07 12:44:07 -0500

    Initial import

---
+++ src/__tests__/App.test.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import * as renderer from 'react-test-renderer';
+
+import App from '../App';
+
+describe('App', () => {
+
+    it('snapshot test', async () => {
+
+        const tree = renderer.create(
+            <App />,
+        ).toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+
+});
